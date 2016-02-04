3bdddc7784adb084341ffb84e057933d4eb1852b
jennmeedo <jennmeedo@gmail.com>
2016-02-03 21:23:30 -0800

    Initial Prism shell and modules

---
+++ ModuleAModule/ModuleAModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleAModule
+{
+    public class ModuleAModule : IModule
+    {
+        private IUnityContainer container;
+        private IRegionManager regionManager;
+
+        public ModuleAModule(IUnityContainer container, IRegionManager regionManager)
+        {
+            this.container = container;
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            container.Resolve<ToolbarView>();
+            container.Resolve<ContentView>();
+            regionManager.RegisterViewWithRegion("ToolbarRegion", typeof( ToolbarView));
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ContentView));
+        }
+    }
+}
---
+++ PrismDemo/UBootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace PrismDemo
+{
+    public class UBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+
+        {
+            return Container.Resolve<Shell>();
+        }
+
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            ModuleCatalog catalog = new ModuleCatalog();
+            catalog.AddModule(typeof (ModuleAModule.ModuleAModule), InitializationMode.WhenAvailable);
+            return catalog;
+        }
+    }
+}
